cebbf6d3273890b066577dd3d7135bd80daebaaf
Eric Nelson <ernelson12o0@gmail.com>
2023-03-26 11:25:36 -0400

    Lab Complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breeds = document.getElementById('dog-breeds');
+const newImg = document.createElement('img');
+newImg.setAttribute('src', imgUrl)
+breeds.appendChild(newImg)
